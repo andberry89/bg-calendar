23a9f35ddd65efe3fe0c900b357f210a734bc9a4
Andrew Berry <andberry89@gmail.com>
2026-03-16 14:16:16 -0400

    fix: guard firebase initialization to prevent duplicate app instances

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -10,6 +10,8 @@ const firebaseConfig = {
   appId: process.env.VUE_APP_FIREBASE_APP_ID,
 };
 
-firebase.initializeApp(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 export const db = firebase.firestore();
