3e6ac00059c0efdd9839b60c21093869c67cb945
Andrew Berry <andberry89@gmail.com>
2026-03-23 22:06:03 -0400

    refactor(calendar): move event validation to feature layer with declarative rules

---
+++ src/features/calendar/utils/eventValidation.ts
@@ -0,0 +1,62 @@
+import type { EventType, Staff } from '@/types/calendar';
+
+type DraftEventType = EventType | '';
+
+interface EventValidationInput {
+  type: DraftEventType;
+  start: string;
+  end: string;
+  details: string;
+  staff: Staff[];
+}
+
+const detailOptionalTypes: ReadonlySet<EventType> = new Set([
+  'Vacation',
+  'Sick Time',
+  'Birthday',
+  'Comp Day'
+]);
+
+const staffOptionalTypes: ReadonlySet<EventType> = new Set(['Holiday', 'C/D Event']);
+
+function requiresDetails(type: DraftEventType): boolean {
+  return type !== '' && !detailOptionalTypes.has(type);
+}
+
+function requiresStaff(type: DraftEventType): boolean {
+  return type !== '' && !staffOptionalTypes.has(type);
+}
+
+export function validateEvent(input: EventValidationInput): string[] {
+  const errors: string[] = [];
+
+  if (!input.type) {
+    errors.push('Event Type');
+  }
+
+  if (!input.start) {
+    errors.push('Start Date');
+  }
+
+  if (!input.end) {
+    errors.push('End Date');
+  }
+
+  if (requiresDetails(input.type) && !input.details) {
+    errors.push('Event Details');
+  }
+
+  if (input.type === 'Comp Day') {
+    if (input.staff.length !== 1) {
+      errors.push('Exactly 1 Staff Member');
+    }
+
+    return errors;
+  }
+
+  if (requiresStaff(input.type) && input.staff.length === 0) {
+    errors.push('Staff');
+  }
+
+  return errors;
+}
