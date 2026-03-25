f88cc88a4b275e5b3db39dc6f6b8fec00c8853c2
Andrew Berry <andberry89@gmail.com>
2026-03-25 15:20:57 -0400

    feat(calendar): map event types to styling tokens

--- src/features/calendar/utils/eventTypeConfig.ts
+++ src/features/calendar/utils/eventTypeConfig.ts
@@ -7,40 +7,49 @@ export type EventTypeField = 'details' | 'staff';
 export interface EventTypeConfig {
   requiredFields: EventTypeField[];
   showsSpecialDayReminder: boolean;
+  colorVar: string;
 }
 
 const eventTypeConfig: Readonly<Record<EventType, EventTypeConfig>> = {
   Vacation: {
     requiredFields: ['staff'],
-    showsSpecialDayReminder: false
+    showsSpecialDayReminder: false,
+    colorVar: '--event-type-vacation'
   },
   'Sick Time': {
     requiredFields: ['details', 'staff'],
-    showsSpecialDayReminder: false
+    showsSpecialDayReminder: false,
+    colorVar: '--event-type-sick-time'
   },
   Holiday: {
     requiredFields: [],
-    showsSpecialDayReminder: false
+    showsSpecialDayReminder: false,
+    colorVar: '--event-type-holiday'
   },
   'Press Trip': {
     requiredFields: ['details', 'staff'],
-    showsSpecialDayReminder: true
+    showsSpecialDayReminder: true,
+    colorVar: '--event-type-press-trip'
   },
   'Auto Show': {
     requiredFields: ['details', 'staff'],
-    showsSpecialDayReminder: true
+    showsSpecialDayReminder: true,
+    colorVar: '--event-type-auto-show'
   },
   Birthday: {
     requiredFields: ['staff'],
-    showsSpecialDayReminder: false
+    showsSpecialDayReminder: false,
+    colorVar: '--event-type-birthday'
   },
   'C/D Event': {
     requiredFields: ['details'],
-    showsSpecialDayReminder: true
+    showsSpecialDayReminder: true,
+    colorVar: '--event-type-cd-event'
   },
   'Comp Day': {
     requiredFields: ['staff'],
-    showsSpecialDayReminder: true
+    showsSpecialDayReminder: true,
+    colorVar: '--event-type-comp-day'
   }
 };
 
