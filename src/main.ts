5e83f716560292304f6b45837126ed64a8e45e3b
Andrew Berry <andberry89@gmail.com>
2026-04-12 00:25:53 -0400

    refactor(directives): remove unused click-outside directive

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,10 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import './styles/main.scss';
 import VueTextareaAutosize from 'vue-textarea-autosize';
-import ClickOutside from '@/directives/click-outside';
 
 const app = createApp(App);
 const pinia = createPinia();
 
-app.directive('click-outside', ClickOutside);
 app.use(pinia);
 app.use(VueTextareaAutosize);
 app.mount('#app');
